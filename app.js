8c755df9583560c9beab3eca88062f754c9297ba
Anton Lovkin <anton28111987@gmail.com>
2023-07-26 16:13:10 +0300

    added edit functionality

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import './style.css';
-import { notesData } from './data';
+import { data } from './data';
 // import { addNote, createNote, deleteNote } from './api';
 import { v4 as uuidv4 } from 'uuid';
 
+let notesData = data;
+
+
+renderNotesList(notesData);
+
 const createNote = (name, content, category) => {
     const id = uuidv4();
-    return { id, name, content, category, createdAt: new Date().toISOString(), isActive: true };
+    return { id, name, content, category, createdAt: new Date().toISOString(), isArchived: false };
 };
 
 const addNote = (note, list) => {
@@ -23,18 +28,100 @@ const deleteNote = (list, id) => {
     }
 };
 
+const archiveNote = (list, id) => {
+  const notesList = list.map((note) => {
+    if (note.id == id) {
+      return {...note, isArchived: !note.isArchived}
+    }
+    return note;
+  });
+  return notesList;
+};
+
+// Function to show the edit form with existing note content
+function showEditForm(note) {
+  const editForm = document.createElement('form');
+  editForm.setAttribute("id",'editForm' )
+  editForm.innerHTML = `
+      <label for="editName">Note Name:</label>
+      <input type="text" id="editName" required value="${note.name}">
+      <label for="editContent">Note Content:</label>
+      <input type="text" id="editContent" required value="${note.content}">
+      <label for="editCategory">Category:</label>
+      <select id="editCategory" required>
+        <option value="Task">Task</option>
+        <option value="Random Thought">Random Thought</option>
+        <option value="Idea">Idea</option>
+      </select>
+      <button type="submit">Save</button>
+      <button type="button" onclick="cancelEdit()">Cancel</button>
+  `;
+
+  editForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const editName = document.getElementById('editName').value;
+    const editContent = document.getElementById('editContent').value;
+    const editCategory = document.getElementById('editCategory').value;
+    
+    if (editContent.trim() !== '') {
+      editNoteContent(note.id, { editName, editContent, editCategory });
+      closeEditForm();
+    }
+  });
+
+  const existingEditForm = document.getElementById('editForm');
+  console.log(existingEditForm)
+  if (existingEditForm) {
+    existingEditForm.replaceWith(editForm);
+  } else {
+    document.body.appendChild(editForm);
+  }
+}
+
+// Function to update the note's content
+function editNoteContent(noteId, editedContent) {
+  const note = notesData.find((note) => note.id === noteId);
+  if (note) {
+    const { editName, editContent, editCategory } = editedContent;
+    note.name = editName;
+    note.content = editContent;
+    note.category = editCategory;
+    console.log(`Note with ID ${noteId} is edited.`);
+    renderNotesList(notesData);
+  } else {
+    console.log(`Note with ID ${noteId} not found.`);
+  }
+}
+
+// Function to close the edit form
+function closeEditForm() {
+  const editForm = document.getElementById('editForm');
+  console.log(editForm)
+  if (editForm) {
+    editForm.remove();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  addEventListenersToButtons();
+  renderNotesList(notesData);
+});
+
+
 const formEl = document.getElementById('addNoteForm');
 
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit (event) {
   event.preventDefault();
+
   const noteName = document.getElementById('noteName').value;
   const noteContent = document.getElementById('noteContent').value;
   const noteCategory = document.getElementById('noteCategory').value;
   
   const newNote = createNote(noteName, noteContent, noteCategory);
-  console.log(newNote)
+
   addNote(newNote, notesData);
 
   renderNotesList(notesData);
@@ -44,15 +131,45 @@ function onFormSubmit (event) {
   document.getElementById('noteName').value = ''
   document.getElementById('noteContent').value = '';
   document.getElementById('noteCategory').value = 'Task';
-}
+};
 
 // ------------------------------------
+function addEventListenersToButtons() {
+  const listEl = document.getElementById('notesList');
+
+  listEl.addEventListener('click', function (event) {
+    const target = event.target;
+    const noteId = target.parentElement.getAttribute('id');
+
+    if (target.classList.contains('archiveButton')) {
+      // console.log('before --', notesData)
+      notesData = archiveNote(notesData, noteId);
+      // console.log('after --',notesData)
+      renderNotesList(notesData);
+      console.log(`Archive Button clicked for note ID: ${noteId}`);
+
+    } else if (target.classList.contains('deleteButton')) {
+      deleteNote(notesData, noteId);
+      renderNotesList(notesData);
+      console.log(`Delete Button clicked for note ID: ${noteId}`);
+
+    } else if (target.classList.contains('editButton')) {
+      const note = notesData.find((note) => note.id == noteId);
+      if (note) {
+        showEditForm(note);
+      }
+      console.log(`Edit Button clicked for note ID: ${noteId}`);
+    }
+  });
+};
 
 function renderNotesList(data) {
   const listEl = document.getElementById('notesList');
+  listEl.innerHTML = ''
   // console.log(listEl)
+  const activeNotes = data.filter(note => !note.isArchived)
 
-  const notesListMarkup = data.map(note => {
+  const notesListMarkup = activeNotes.map(note => {
     const { id, createdAt, content, category, name } = note;
     return `<li id="${id}">
               <span>${name}</span>
@@ -67,33 +184,12 @@ function renderNotesList(data) {
   });
 
   listEl.innerHTML = notesListMarkup.join();
+  // addEventListenersToButtons();
 };
 
 //-------------------------------------------------
 
-function addEventListenersToButtons() {
-  const listEl = document.getElementById('notesList');
-
-  listEl.addEventListener('click', function(event) {
-    const target = event.target;
-    const noteId = target.parentElement.getAttribute('id');
-
-    if (target.classList.contains('archiveButton')) {
-      console.log(`Archive Button clicked for note ID: ${noteId}`);
-
-    } else if (target.classList.contains('deleteButton')) {
-      deleteNote(notesData, noteId);
-      renderNotesList(notesData);
-      console.log(`Delete Button clicked for note ID: ${noteId}`);
-    } else if (target.classList.contains('editButton')) {
-      deleteNote(notesData, noteId);
-      renderNotesList(notesData);
-      console.log(`Edit Button clicked for note ID: ${noteId}`);
-    }
-  });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   addEventListenersToButtons();
-  renderNotesList(notesData)
+  // renderNotesList(notesData)
 });
